b62602841643c761987afb4bc974172ec8dca2c6
Evan <schafer.evan.r@gmail.com>
2020-05-04 14:33:00 -0700

    Make occupant buttons update fee totals
    Clicking add or subcract adult or kid now adds the associated rent, service, and occupancy fees and refreshes what is displayed on the screen

--- client/src/Fees.jsx
+++ client/src/Fees.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 function Fees(props) {
   const p = props;
+  const baseRent = p.rent;
   return (
     <div id="fees">
-      <h4>Rent per Night:</h4>
+      <h2>Rent Due:</h2>
       <h2>{p.rent}</h2>
+      <h4>Rent per Night:</h4>
+      <h4>{baseRent}</h4>
       <h4>Service Fee:</h4>
-      <h2>{p.service}</h2>
+      <h4>{p.service}</h4>
       <h4>Cleaning Fee:</h4>
-      <h2>{p.cleaning}</h2>
+      <h4>{p.cleaning}</h4>
       <h4>Occupancy Fee:</h4>
-      <h2>{p.occupancy}</h2>
+      <h4>{p.occupancy}</h4>
     </div>
   );
 }
