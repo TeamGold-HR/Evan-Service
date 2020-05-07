7bfe3919ddbdc4b77ca46f0a7bb88b57807d7ae8
Evan <schafer.evan.r@gmail.com>
2020-05-06 23:39:32 -0700

    Polish styling and add more calendar functionality
    Component was styled to more closely resemble the original airbnb component. Additionally, the calendar has been given the functionality to display selected dates in a human-readable format, as well as store data for the dates in a format convenient for sending database queries.
    The calendar can also detect the user trying to select an end date that is further back than the start date, but currently this does not work with dates outside the current month.

--- client/src/Occupants.jsx
+++ client/src/Occupants.jsx
@@ -12,35 +12,43 @@ function Occupants(props) {
   };
   return (
     <div id="occupants">
-      <div id="non-infants">
-        <h5>Maximum non-Infants: {p.nonInfants}</h5>
-      </div>
       <div id="adults">
-        <span className="occupant-tracker">
-          <h2 className="occupant">Adults</h2>
-          <button type="button" className="decrement" id="decrease-adults" onClick={decrease}>-</button>
-          <h2 className="tally">{p.adultsSelected}</h2>
-          <button type="button" className="increment" id="increase-adults" onClick={increase}>+</button>
-        </span>
-        <h5 className="maximum">Maximum Adults: {p.maxAdults}</h5>
+        <div className="occupant-tracker">
+          <div className="occupant-type">
+            <h2 className="occupant">Adults</h2>
+          </div>
+          <div className="occupant-adjustor">
+            <button type="button" className="decrement" id="decrease-adults" onClick={decrease}>-</button>
+            <h2 className="tally">{p.adultsSelected}</h2>
+            <button type="button" className="increment" id="increase-adults" onClick={increase}>+</button>
+          </div>
+        </div>
       </div>
       <div id="children">
-        <span className="occupant-tracker">
-          <h2 className="occupant">Children (age 2 - 12)</h2>
-          <button type="button" className="decrement" id="decrease-children" onClick={decrease}>-</button>
-          <h2 className="tally">{p.childrenSelected}</h2>
-          <button type="button" className="increment" id="increase-children" onClick={increase}>+</button>
-        </span>
-        <h5>Maximum Children: {p.maxChildren}</h5>
+        <div className="occupant-tracker">
+          <div className="occupant-type">
+            <p className="occupant bold">Children</p>
+            <p>age 2 - 12</p>
+          </div>
+          <div className="occupant-adjustor">
+            <button type="button" className="decrement" id="decrease-children" onClick={decrease}>-</button>
+            <h2 className="tally">{p.childrenSelected}</h2>
+            <button type="button" className="increment" id="increase-children" onClick={increase}>+</button>
+          </div>
+        </div>
       </div>
       <div id="infants">
-        <span className="occupant-tracker">
-          <h2 className="occupant">Infants (under age 2)</h2>
-          <button type="button" className="decrement" id="decrease-infants" onClick={decrease}>-</button>
-          <h2 className="tally">{p.infantsSelected}</h2>
-          <button type="button" className="increment" id="increase-infants" onClick={increase}>+</button>
-        </span>
-        <h5>Maximum Infants: {p.maxInfants}</h5>
+        <div className="occupant-tracker">
+          <div className="occupant-type">
+            <p className="occupant bold">Infants</p>
+            <p>under age 2</p>
+          </div>
+          <div className="occupant-type">
+            <button type="button" className="decrement" id="decrease-infants" onClick={decrease}>-</button>
+            <h2 className="tally">{p.infantsSelected}</h2>
+            <button type="button" className="increment" id="increase-infants" onClick={increase}>+</button>
+          </div>
+        </div>
       </div>
     </div>
   );
