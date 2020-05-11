66302b2132f44537caf08a1b06bfd66b49a1f1e3
Evan <schafer.evan.r@gmail.com>
2020-05-11 10:57:01 -0700

    Update to a superior port number

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3007;
 const db = require('../database/index.js');
 
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/:listingId', express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.redirect(301, 'http://127.0.0.1:3002/0');
+  res.redirect(301, 'http://127.0.0.1:3007/0');
 });
 
 app.get('/list/:listingId', (req, res) => {
@@ -38,5 +38,5 @@ app.get('/list/:listingId', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('WOW WOW 3000');
+  console.log('WOW WOW 3007');
 });
